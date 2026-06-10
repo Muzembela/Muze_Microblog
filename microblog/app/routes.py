4e7e2ce253e8cd26bc41895ee3d031640ee4ec33
Manuel Muzembela <mmuzembe@student.42luanda.com>
2026-06-10 17:44:16 +0100

    login form created

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,8 +1,29 @@
 
 from app import app
+from flask import render_template
+from app.forms import LoginForm
+from config import Config
+
 
 @app.route('/')
 @app.route('/index')
 
 def index() :
-	return 'Hello Muzembela';
+	user = {'username' : 'Muzembela'}
+	posts = [ 
+			{
+				'author' : {'username' : 'Manuel'},
+				'body' : 'Beatiful Day in Belas Shopping!'
+			},
+			{
+				'author' : {'username' : 'Bernardo'},
+				'body' : 'The avengers movie are good!'
+			}
+			]
+	return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login')
+def login() :
+	form = LoginForm()
+	return render_template('login.html', title = 'Sign In', form=form)
